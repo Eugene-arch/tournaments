1cafb8d0b9fae42d8071fa38329c570870f66eba
Eugene <74623418+Eugene-arch@users.noreply.github.com>
2021-05-23 21:46:49 +0300

    Created block 'Wet sand' (reeds can be placed on it without water);

---
+++ src/main/java/com/eugenearch/tournaments/blocks/WetSand.java
@@ -0,0 +1,53 @@
+package com.eugenearch.tournaments.blocks;
+
+import com.eugenearch.tournaments.Main;
+import com.eugenearch.tournaments.utils.interfaces.IHasModel;
+import com.eugenearch.tournaments.utils.registry.BlockRegistry;
+import com.eugenearch.tournaments.utils.registry.ItemRegistry;
+import net.minecraft.block.BlockBreakable;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IPlantable;
+
+import java.util.List;
+
+public class WetSand extends BlockBreakable implements IHasModel {
+    public WetSand(String name) {
+        super(Material.SAND, false);
+
+        setRegistryName(name);
+        setUnlocalizedName(name);
+
+        setCreativeTab(Main.TOURNAMENTS_TAB);
+
+        BlockRegistry.BLOCKS.add(this);
+        ItemRegistry.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+    }
+
+    @Override
+    public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, IPlantable plantable) {
+        IBlockState plant = plantable.getPlant(world, pos.offset(direction));
+        return plant.getBlock().equals(Blocks.REEDS);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
+        tooltip.add(I18n.format("tile.wet_sand.tooltip"));
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+}
--- src/main/java/com/eugenearch/tournaments/utils/registry/BlockRegistry.java
+++ src/main/java/com/eugenearch/tournaments/utils/registry/BlockRegistry.java
@@ -1,9 +1,6 @@
 package com.eugenearch.tournaments.utils.registry;
 
-import com.eugenearch.tournaments.blocks.HealingRoad;
-import com.eugenearch.tournaments.blocks.LeafEscapeBlock;
-import com.eugenearch.tournaments.blocks.SpleefBlock;
-import com.eugenearch.tournaments.blocks.TNTRunBlock;
+import com.eugenearch.tournaments.blocks.*;
 import net.minecraft.block.Block;
 
 import java.util.ArrayList;
@@ -16,4 +13,5 @@ public class BlockRegistry {
     public static Block TNTRUN_BLOCK = new TNTRunBlock("tntrun_block");
     public static Block LEAFESCAPE_BLOCK = new LeafEscapeBlock("leafescape_block");
     public static Block HEALER_BLOCK = new HealingRoad("healer_block");
+    public static Block WET_SAND = new WetSand("wet_sand");
 }
