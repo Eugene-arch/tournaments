950d2fdf9a37173bf2e6ae1b726d053df6a1303b
Prototype <17ruslan1997@gmail.com>
2021-06-29 22:10:45 +0300

    code review

--- src/main/java/com/eugenearch/tournaments/items/ItemSlimeChunkFounder.java
+++ src/main/java/com/eugenearch/tournaments/items/ItemSlimeChunkFounder.java
@@ -10,26 +10,29 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.List;
 
 public class ItemSlimeChunkFounder extends Item implements IHasModel {
+
+    public static final String TAG_LAST_SLIME_CHUNK_STATUS = "isInSlimeChunk";
+
     private String name;
-    private int chunk_key;
 
     public ItemSlimeChunkFounder(String name) {
-        super();
-
         this.name = name;
         setRegistryName(name);
         setUnlocalizedName(name);
         setCreativeTab(Main.TOURNAMENTS_TAB);
 
         ItemRegistry.ITEMS.add(this);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
@@ -43,46 +46,74 @@ public class ItemSlimeChunkFounder extends Item implements IHasModel {
         Main.proxy.registerItemRenderer(this, 0, "inventory");
     }
 
-    private boolean getChunk_key(ItemStack stack) {
-        if (!stack.hasTagCompound()) {
-            setChunk_key(stack, false);
-            return false;
-        }
-        NBTTagCompound tag = stack.getTagCompound();
-        return tag.getBoolean("isInSlimeChunk");
+    private boolean getLastChunkStatus(ItemStack stack) {
+        return ensureTagCompound(stack).getBoolean(TAG_LAST_SLIME_CHUNK_STATUS);
     }
 
-    private void setChunk_key(ItemStack stack, boolean isSlimeChunk) {
-        NBTTagCompound tag;
-        if (!stack.hasTagCompound()) {
-            tag = new NBTTagCompound();
-        } else {
-            tag = stack.getTagCompound();
-        }
-        tag.setBoolean("isInSlimeChunk", isSlimeChunk);
-        stack.setTagCompound(tag);
+    private void setLastChunkStatus(ItemStack stack, boolean isSlimeChunk) {
+        ensureTagCompound(stack).setBoolean(TAG_LAST_SLIME_CHUNK_STATUS, isSlimeChunk);
     }
 
-    @Override
-    public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
-        if (!world.isRemote && entity instanceof EntityPlayer && ((EntityPlayer) entity).getHeldItemOffhand() == stack) {
-            if (entity != null) {
-                Chunk chunk = world.getChunkFromChunkCoords(MathHelper.floor(entity.posX) >> 4, MathHelper.floor(entity.posZ) >> 4);
-                boolean isSlimeChunkPrev = this.getChunk_key(stack);
+    public static NBTTagCompound ensureTagCompound(ItemStack itemStack) {
+        NBTTagCompound compound = itemStack.getTagCompound();
+        if (compound == null) {
+            compound = new NBTTagCompound();
+            itemStack.setTagCompound(compound);
+        }
+        return compound;
+    }
+
+    @SubscribeEvent
+    public void onEnteringChunk(EntityEvent.EnteringChunk event) {
+
+        final Entity entity = event.getEntity();
+        if (entity.getEntityWorld().isRemote) return;
+
+        if (entity instanceof EntityPlayer) {
+
+            ItemStack offhand = ((EntityPlayer) entity).getHeldItemOffhand();
+            if (!offhand.isEmpty() && offhand.getItem() instanceof ItemSlimeChunkFounder) {
+
+                Chunk chunk = entity.getEntityWorld().getChunkFromChunkCoords(event.getNewChunkX(), event.getNewChunkZ());
+                boolean isSlimeChunkPrev = this.getLastChunkStatus(offhand);
                 boolean isSlimeChunkNew = chunk.getRandomWithSeed(987234911L).nextInt(10) == 0;
 
                 if (isSlimeChunkPrev != isSlimeChunkNew) {
-                    this.setChunk_key(stack, isSlimeChunkNew);
-
-                    if (isSlimeChunkPrev == false) {
+                    this.setLastChunkStatus(offhand, isSlimeChunkNew);
+                    if (!isSlimeChunkPrev) {
                         entity.sendMessage(new TextComponentTranslation("item." + name + ".messages.slimechunk_enter"));
                     } else {
                         entity.sendMessage(new TextComponentTranslation("item." + name + ".messages.slimechunk_leave"));
                     }
                 }
-            } else {
-                this.setChunk_key(stack, false);
+
             }
+
         }
+
     }
+
+//    @Override
+//    public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
+//        if (!world.isRemote && entity instanceof EntityPlayer && ((EntityPlayer) entity).getHeldItemOffhand() == stack) {
+//            if (entity != null) {
+//                Chunk chunk = world.getChunkFromChunkCoords(MathHelper.floor(entity.posX) >> 4, MathHelper.floor(entity.posZ) >> 4);
+//                boolean isSlimeChunkPrev = this.getChunk_key(stack);
+//                boolean isSlimeChunkNew = chunk.getRandomWithSeed(987234911L).nextInt(10) == 0;
+//
+//                if (isSlimeChunkPrev != isSlimeChunkNew) {
+//                    this.setChunk_key(stack, isSlimeChunkNew);
+//
+//                    if (isSlimeChunkPrev == false) { <- WTF
+//                        entity.sendMessage(new TextComponentTranslation("item." + name + ".messages.slimechunk_enter"));
+//                    } else {
+//                        entity.sendMessage(new TextComponentTranslation("item." + name + ".messages.slimechunk_leave"));
+//                    }
+//                }
+//            } else {
+//                this.setChunk_key(stack, false);
+//            }
+//        }
+//    }
+
 }
